9e517e88d2a5b0337d99641a0e39284ffd5ee336
Anraaa <aqlaharun@gmail.com>
2024-10-23 10:26:30 +0700

    now

---
+++ src/praktikum/sesi5/ClassBintangFilm.java
@@ -0,0 +1,32 @@
+package praktikum.sesi5;
+
+class BintangFilm {
+    private String nama;
+    private Boolean pria;
+
+    BintangFilm(String nama, Boolean pria){
+        this.nama = nama;
+        this.pria = pria;
+    }
+
+    String perolehNama(){
+        return (nama);
+    }
+
+    String perolehJenisKelamin (){
+        if (pria)
+            return ("Pria");
+        else    
+            return ("Wanita");
+    }
+}
+
+public class ClassBintangFilm {
+    public static void main(String[] args) {
+        BintangFilm siA = new BintangFilm("Budi", true);
+        BintangFilm siB = new BintangFilm("Hani", false);
+    
+    System.out.println("Nama: " + siA.perolehNama() + " Jenis Kelamin: " + siA.perolehJenisKelamin());
+    System.out.println("Nama: " + siB.perolehNama() + " Jenis Kelamin: " + siB.perolehJenisKelamin());
+    }
+}
